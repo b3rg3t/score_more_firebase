3d34d1dca26b857d11136705750ce87dab29e8cd
b3rg3t <berget_90db@hotmail.com>
2023-06-03 19:16:14 +0200

    css update for layout

--- src/components/layout/accordion/Accordion.tsx
+++ src/components/layout/accordion/Accordion.tsx
@@ -11,23 +11,23 @@ const Accordion = ({ title, children }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Collapsible
-      onOpening={() => setIsOpen(true)}
-      onClosing={() => setIsOpen(false)}
-      trigger={
-        <div className="d-flex justify-content-between accordion-header">
-          <span className="fw-bold">{title}</span>
-          <span className={isOpen ? "rotate" : "arrow-down"}>
-            <FaChevronDown />
-          </span>
-        </div>
-      }
-      classParentString="hallelulja"
-      triggerClassName="hallelulja"
-      contentOuterClassName="border-top border-bottom mb-1"
-    >
-      {children}
-    </Collapsible>
+    <div className="bg-white rounded p-1 mb-1 box-shadow text-dark">
+      <Collapsible
+        onOpening={() => setIsOpen(true)}
+        onClosing={() => setIsOpen(false)}
+        trigger={
+          <div className="d-flex justify-content-between accordion-header">
+            <span className="fw-bold">{title}</span>
+            <span className={isOpen ? "rotate" : "arrow-down"}>
+              <FaChevronDown />
+            </span>
+          </div>
+        }
+        classParentString="accordion"
+      >
+        {children}
+      </Collapsible>
+    </div>
   );
 };
 
